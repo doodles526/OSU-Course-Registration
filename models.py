7a1da14d2e42ca269530a4a69ed8456953efd423
Joshua Deare <doodles@doodles.(none)>
2013-03-07 23:06:16 -0800

    progress: 1. finished models.py NEEDS WORK: 1. re-write scraper to output correctly

--- models.py
+++ models.py
@@ -11,11 +11,11 @@ class Course(db.Model):
     credits = db.Column(db.Integer(1), nullable = False)
 
     def __init__(self, *args, **kwargs):
-        subject = kwargs['shortname'][:-3]
-        number = int(kwargs['shortname'][-3:])
-        description = kwargs['description']
-        fullname = kwargs['fullname']
-        shortname = kwargs['shortname']
+        self.subject = kwargs['shortname'][:-3]
+        self.number = int(kwargs['shortname'][-3:])
+        self.description = kwargs['description']
+        self.fullname = kwargs['fullname']
+        self.shortname = kwargs['shortname']
 
     def __repr__(self):
         return '<Course %r>' $ (self.shortname)
@@ -23,7 +23,7 @@ class Course(db.Model):
 class CourseInstance(db.Model):
     __tablename__ = 'course_instances'
     id = db.Column(db.Intger(), primary_key = True)
-    course = db.Column(db.Integer(), db.ForeignKey('courses.id'))
+    cid = db.Column(db.Integer(), db.ForeignKey('courses.id'))
     crn = db.Column(db.Integer(), unique = True, nullable = False)
     term = db.Column(db.String, nullable = False)
     section = db.Column(db.Integer(3), nullable = False)
@@ -49,15 +49,22 @@ class CourseInstance(db.Model):
     comments = db.Column(db.String())
     
     def __init__(self, *args, **kwargs):
-        
-
-
-
-
-
-
-
-
-
-
-
+        self.cid = kwargs['cid']
+        self.crn = kwargs['crn']
+        self.term = kwargs['term']
+        self.section = kwargs['section']
+        self.parent_crn = kwargs['parent_crn']
+        self.instructor = kwargs['instructor']
+        self.days = kwargs['days']
+        self.time_start = kwargs['time_start']
+        self.time_end = kwargs['time_end']
+        self.campus = kwargs['campus']
+        self.pass_nopass = kwargs['pass_nopass']
+        self.type = kwargs['type']
+        self.cap = kwargs['cap']
+        self.current = kwargs['current']
+        self.wl_cap = kwargs['wl_cap']
+        self.wl_current = kwargs['wl_current']
+        self.fees = kwargs['fees']
+        self.restrictions = kwargs['restrictions']
+        self.comments = kwargs['comments']
